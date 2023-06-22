ca6f241235e34cea55928bd742fdcb10c8c0a13e
arnavk1 <arnavkashyap3108@gmail.com>
2023-06-22 22:11:43 +0530

    first commit

--- script.js
+++ script.js
@@ -14,17 +14,78 @@
 // }
 // localStorage.clear()
 // console.log(typeof(Array.from(localStorage)))
-// document.getElementById("add").addEventListener("click",()=>{
-//     let key = localStorage.length+1
-//     let value1 = false
-//     let value2 = document.getElementById("value").value
-//     let value = {"checked": value1, "value":value2}
-//     // let value = document.getElementById("value").checked
-//     // let newValue = """"""
-//     localStorage.setItem(key,JSON.stringify(value))
-//     document.getElementById("value").value = ""
-// })
-let value = JSON.parse(localStorage.getItem(1))
-value.checked = true
+document.getElementById("add").addEventListener("click", () => {
+    let key = localStorage.length + 1
+    let value1 = false
+    let value2 = document.getElementById("value").value
+    let value = { "checked": value1, "value": value2 }
+    // let value = document.getElementById("value").checked
+    // let newValue = """"""
+    localStorage.setItem(key, JSON.stringify(value))
+    document.getElementById("value").value = ""
+    document.getElementById("lower").innerHTML = ""
+    showItems()
+})
+// let value = JSON.parse(localStorage.getItem(3))
+// value.checked = true
 
-console.log(localStorage.setItem(1,JSON.stringify(value)))
+// console.log(localStorage.setItem(3,JSON.stringify(value)))
+
+showItems()
+
+function showItems() {
+    document.getElementById("lower").innerHTML = ""
+    for (i = 1; i <= localStorage.length; i++) {
+        let div = document.createElement('div')
+        div.setAttribute('class', 'data')
+        let data = JSON.parse(localStorage.getItem(i)).value
+        div.innerHTML = `<input type='checkbox' id='check${i}' class="checking" onClick="cBoxFunc(this)"> <label for='check${i}'> ${data} </label>`
+        // console.log(JSON.parse(localStorage.getItem(i)).checked === true)
+        if (JSON.parse(localStorage.getItem(i)).checked === true) {
+            // document.querySelector(`check${i}`).checked = true
+            continue;
+        }
+        document.getElementById("lower").append(div)
+
+
+
+
+        // let check = document.createElement('input')
+        // check.setAttribute('type', 'checkbox')
+        // check.setAttribute('id', 'checking')
+        // check.setAttribute('class', 'check')
+
+        // let label = document.createElement('p')
+        // label.setAttribute('class', 'list')
+        // label.setAttribute('for', 'checking')
+        // label.innerHTML = JSON.parse(localStorage.getItem(i)).value
+        // document.getElementById("data").append(check)
+        // document.getElementById("data").append(label)
+    }
+}
+
+
+function cBoxFunc(ele){
+    // const cbox = document.querySelector(".checking")
+    let cNum = ele.id.slice(5,);
+    if (ele.checked) {
+        let data = JSON.parse(localStorage.getItem(cNum))
+        data.checked = true
+        localStorage.setItem(cNum, JSON.stringify(data))
+        showItems()
+    } else {
+        console.log("Checkbox is not checked..");
+    }
+}
+// cbox.addEventListener("change", function () {
+    
+// })
+
+
+
+// for(i=0;i<3;i++){
+//     let div = document.createElement('div')
+//     div.setAttribute("class","test")
+//     div.innerHTML="<h1 class='lolve'>hello"+i+" how are you</h1>"
+//     document.getElementById("lower").append(div)
+// }
